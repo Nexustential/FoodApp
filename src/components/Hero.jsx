ae5b3a479f67c749c70fcef1545555de39bd46e1
Ryan Lewis <rclewis369@outlook.com>
2022-12-29 18:55:55 -0500

    Added promo cards and food/category arrays, set up filter buttons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,10 +9,10 @@ const Hero = () => {
 					<h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>The <span className='text-orange-500'>Best</span></h1>
 					<h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'><span className='text-orange-500'>Food</span> Delivered</h1>
 				</div>
-				<img className='w-full max-h-[500px] object-cover' src="https://images.pexels.com/photos/3186654/pexels-photo-3186654.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+				<img className='w-full max-h-[500px] object-cover' src="https://images.pexels.com/photos/3186654/pexels-photo-3186654.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="/" />
 			</div>
     </div>
   )
 }
 
-export default Hero
+export default Hero;
