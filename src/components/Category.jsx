ea37a19a3ee9a945c6ed7ff770b066f26239510a
Ryan Lewis <rclewis369@outlook.com>
2023-01-13 15:39:32 -0500

    minor styling

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -8,7 +8,7 @@ const Category = () => {
 			{/* Categories */}
 			<div className='grid grid-cols-2 md:grid-cols-4 gap-6 py-6'>
 				{categories.map((type, index) => (
-					<div key={index} className='bg-gray-100 rounded-lg p-4 flex justify-between items-center'>
+					<div key={index} className='bg-gray-100 rounded-lg p-4 flex justify-between items-center hover:scale-105 duration-300'>
 							<h2 className='font-bold sm:text-xl'>{type.name}</h2>
 							<img className='w-20' src={type.image} alt={type.name} />
 					</div>
